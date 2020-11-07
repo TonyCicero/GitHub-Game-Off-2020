09aefec69180b7c2d6ad884da39dc930d6712371
DESKTOP-Main\TonyC <tony@tcicerodev.com>
2020-11-07 13:22:51 -0500

    Added more commands

--- Assets/console.cs
+++ Assets/console.cs
@@ -7,9 +7,11 @@ public class console : MonoBehaviour
 {
     [SerializeField]
     Text Monitor;
+    private int monitorSize = 7; // number of lines
 
     public consoleVars cVars;
     public bool UsingTerminal;
+
 
     [System.Serializable]
     public class commands
@@ -18,6 +20,22 @@ public class console : MonoBehaviour
         public int reqArgs; // number of required arguments
         public string output; //optional output text to be displayed
     }
+
+    public class LaunchChecklist
+    {
+        public readonly string req_sysState = "11101";
+        public bool passDiag;
+        // binary representation of activated systems
+        //1=active;0=inactive; order= FuelPump>Engine>Navigation>Solar>Communication
+        //example: 11111 = All systems active
+        public bool sys_State; // true if state = 11101
+        public bool Fuel;
+        public bool Batt;
+        public bool Passed; //is ready for launch
+        
+        
+    }
+    LaunchChecklist checklist;
 
     public commands[] cmds;
 
@@ -47,7 +65,14 @@ public class console : MonoBehaviour
             case "CLEAR":
                 CLEARcmd();
                 break;
+            case "CHECKLIST":
+                output = CHECKLISTcmd();
+                break;
+            case "DIAGNOSTIC":
+                output = DIAGNOSTICcmd();
+                break;
             case "HELP":
+                output = HELPcmd(args);
                 break;
             default:
                 return "Invalid Command! for help type 'HELP'\n";
@@ -59,17 +84,141 @@ public class console : MonoBehaviour
 
     string SETcmd(string[] args)
     {
-        return null;
+        if (args.Length > 1)
+        {
+            switch (args[1])
+            {
+                case "Throttle":
+                    int val = int.Parse(args[2]);
+                    if (val >= 0 && val <= 100)
+                    {
+                        cVars.Throttle = val;
+                        return "Throttle set to " + val + "%\n";
+                    }
+                    else
+                    {
+                        return "Invalid Value. Must be between 0 and 100\n";
+                    }
+                case "FuelPump":
+                    if (args[2] == "ON")
+                    {
+                        cVars.FuelPump = true;
+                        return "Fuel Pump set to ON\n";
+                    }else if (args[2] == "OFF")
+                    {
+                        cVars.FuelPump = false;
+                        return "Fuel Pump set to OFF\n";
+                    }
+                    else
+                    {
+                        return "Invalid Input. Expecting 'ON' or 'OFF'";
+                    }
+                case "Engine":
+                    if (args[2] == "ON")
+                    {
+                        cVars.Engine = true;
+                        return "Engine set to ON\n";
+                    }
+                    else if (args[2] == "OFF")
+                    {
+                        cVars.Engine = false;
+                        return "Engine set to OFF\n";
+                    }
+                    else
+                    {
+                        return "Invalid Input. Expecting 'ON' or 'OFF'";
+                    }
+                case "Navigation":
+                    if (args[2] == "ON")
+                    {
+                        cVars.Navigation = true;
+                        return "Navigation system set to ON\n";
+                    }
+                    else if (args[2] == "OFF")
+                    {
+                        cVars.Navigation = false;
+                        return "Navigation system set to OFF\n";
+                    }
+                    else
+                    {
+                        return "Invalid Input. Expecting 'ON' or 'OFF'";
+                    }
+                case "Solar":
+                    if (args[2] == "ON")
+                    {
+                        cVars.Solar = true;
+                        return "Solar set to ON\n";
+                    }
+                    else if (args[2] == "OFF")
+                    {
+                        cVars.Solar = false;
+                        return "Solar set to OFF\n";
+                    }
+                    else
+                    {
+                        return "Invalid Input. Expecting 'ON' or 'OFF'";
+                    }
+                    case "Communication":
+                    if (args[2] == "ON")
+                    {
+                        cVars.Communication = true;
+                        return "Communication system set to ON\n";
+                    }else if (args[2] == "OFF")
+                    {
+                        cVars.Communication = false;
+                        return "Communication system set to OFF\n";
+                    }
+                    else
+                    {
+                        return "Invalid Input. Expecting 'ON' or 'OFF'";
+                    }
+                default:
+                    return "Invalid Variable\n";
+            }
+        }
+        else
+        {
+            return "<b>No Variable specified!</b>\n";
+        }
     }
 
     string GETcmd(string[] args)
     {
-        return null;
+        if (args.Length > 1)
+        {
+            switch (args[1])
+            {
+                case "Throttle":
+                    return "Throttle is currently at " + cVars.Throttle + "%\n";
+                case "Health":
+                    return "Ship Health is currently at " + cVars.Health + "\n";
+                case "Fuel":
+                    return "Fuel level is currently at " + cVars.Fuel + "\n";
+                case "Battery":
+                    return "Battery level is currently at " + cVars.Battery + "\n";
+                case "gForce":
+                    return "Gravitational Force is currently " + cVars.gForce + "N\n";
+
+                default:
+                    return "Invalid Variable\n";
+            }
+        }
+        else
+        {
+            return "<b>No Variable specified!</b>\n";
+        }
     }
 
     string STATUScmd()
     {
-        string output = string.Format("System Status: \nEngine: {0} \nNavigation: {1} \nCommunication: {2} \nSolar: {3}",cVars.Engine,cVars.Navigation,cVars.Communication,cVars.Solar);
+        string sys_state = string.Format("{0}{1}{2}{4}", cVars.FuelPump, cVars.Engine, cVars.Navigation, cVars.Communication, cVars.Solar);
+        sys_state = sys_state.Replace("True", "1");
+        sys_state = sys_state.Replace("False", "0");
+        if (sys_state == checklist.req_sysState) //check if state meet requirement for Launch
+        {
+            checklist.sys_State = true;
+        }
+        string output = string.Format("System Status:\nFuel Pump: {0} \nEngine: {1} \nNavigation: {2} \nCommunication: {3} \nSolar: {4} \nState = {5}",cVars.FuelPump ,cVars.Engine,cVars.Navigation,cVars.Communication,cVars.Solar,sys_state);
         output = output.Replace("True", "<color=green>Active</color>");
         output = output.Replace("False", "<color=red>Inactive</color>");
         return output +"\n";
@@ -79,19 +228,100 @@ public class console : MonoBehaviour
     {
        
         curMonitor = "";
-        Debug.Log(curMonitor);
         Monitor.text = curMonitor;
     }
 
-    string HELPcmd()
+    string CHECKLISTcmd() // displays laucnch checklist
     {
-        return null;
+        string output = string.Format("Launch Checklist:\nDiagnostics: {0} \nFuel: {1} \nBattery: {2} \nSystems Status: {3} \n", checklist.passDiag,checklist.Fuel,checklist.Batt,checklist.sys_State);
+        output = output.Replace("True", "<color=green>Passed</color>");
+        output = output.Replace("False", "<color=red>Failed</color>");
+        if (checklist.passDiag && checklist.Fuel && checklist.Batt && checklist.sys_State)
+        {
+            checklist.Passed = true;
+            output += "<color=green>Ready for launch</color>\n";
+        }
+        else
+        {
+            checklist.Passed = false;
+            output += "<color=red>Not Ready for Launch</color>\n";
+            if (!checklist.sys_State)
+            {
+                output += "<color=red>System State must eqaul " + checklist.req_sysState + "</color>\n";
+            }
+        }
+        return output;
+    }
+
+    string DIAGNOSTICcmd()
+    {
+        checklist.passDiag = true;
+        return "<color=green>No Problems were detected</color>\n";
+    }
+
+    string HELPcmd(string[] args)
+    {
+        if (args.Length > 1)
+        {
+            switch (args[1])
+            {
+                case "SET":
+                    return "<b>SET Command:</b> sets value for a given variable\n" +
+                        "Syntax: SET <var> <value>\n" +
+                        "Variables: 'Throttle','FuelPump','Engine','Navigation','Solar','Communication'\n";
+
+                case "GET":
+                    return "<b>GET Command:</b> gets value for a given variable\n" +
+                        "Syntax: SET <var> \n" +
+                        "Variables: 'Throttle','Health','Fuel','Battery','gForce'\n";
+
+                case "STATUS":
+                    return "<b>STATUS Command:</b> gets statuses of ship systems \n" +
+                        "Syntax: STATUS\n";
+
+                case "CLEAR":
+                    return "<b>CLEAR Command:</b> clears all contents on screen\n" +
+                        "Syntax: CLEAR\n";
+                case "DIAGNOSTIC":
+                    return "<b>DIAGNOSTIC Command:</b> runs system diagnostics test\n" +
+                        "Syntax: DIAGNOSTIC\n";
+                case "CHECKLIST":
+                    return "<b>CHECKLIST Command:</b> runs Launch Checklist\n" +
+                        "Syntax: CHECKLIST\n";
+                default:
+                    return "<b>Command not found!</b> Available commands:'SET','GET','STATUS','CLEAR','DIAGNOSTIC','CHECKLIST'\n";
+            }
+        }
+        else
+        {
+            return "<b>HELP Command:</b> find how to use commands\n" +
+                "Syntax: HELP <command> \n" +
+                "Commands: 'SET','GET','STATUS','CLEAR','DIAGNOSTIC','CHECKLIST'\n";
+        }
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        checklist = new LaunchChecklist();
+    }
 
+    string fit2Screen(string s)
+    {
+        string[] Lines = s.Split('\n');
+        int numLines = Lines.Length;
+        if (numLines< monitorSize) 
+        {
+            return s; //make no changes
+        }
+        int startIndex = numLines - monitorSize;
+        string result = "";
+        for (int i = startIndex; i < numLines; i++)
+        {
+            result += Lines[i] + "\n";
+        }
+
+        return result;
     }
 
     IEnumerator readInput(char c)
@@ -99,14 +329,17 @@ public class console : MonoBehaviour
 
         if (c == '\b') //backspace key
         {
-            curMonitor = curMonitor.Substring(0, curMonitor.Length - 1);
-            UserInput = UserInput.Substring(0, UserInput.Length - 1);
+            Debug.Log("backspace");
+            if (curMonitor.Length > 0) { curMonitor = curMonitor.Substring(0, curMonitor.Length - 1); }
+            if (UserInput.Length > 0) { UserInput = UserInput.Substring(0, UserInput.Length - 1); }
         }
         else if (c == '\n' || c == '\r')//enter or return key hit
         {
             string result = "\n" + execute(UserInput); // execute cmd & display output
             curMonitor += result;
+            curMonitor = fit2Screen(curMonitor);
             UserInput = "";
+            
         }
         else
         {
--- Assets/consoleVars.cs
+++ Assets/consoleVars.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class consoleVars : MonoBehaviour
 {
     public float Throttle; //ship throttle value 0-100
-    public float Health; //ship health value
-    public float gForce; //gravitational force on ship
-    public float Fuel; //ship fuel level
-    public float Battery; //ship battery level
+    public float Health =100; //ship health value
+    public float gForce =9.8f; //gravitational force on ship
+    public float Fuel =100; //ship fuel level
+    public float Battery =100; //ship battery level
+    public bool FuelPump; //Fuel Pump status
     public bool Engine; //Engine System
     public bool Navigation; //Navigation system status
     public bool Solar; //Solar system status
